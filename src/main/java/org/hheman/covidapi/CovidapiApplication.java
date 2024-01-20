013efacc55c7e768adf35a96cc4b14d9ac7ee94f
hheman <3hheman@gmail.com>
2024-01-20 18:08:45 -0500

    working spring app

---
+++ src/main/java/org/hheman/covidapi/CovidapiApplication.java
@@ -0,0 +1,13 @@
+package org.hheman.covidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hheman/covidapi/CovidapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hheman.covidapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
